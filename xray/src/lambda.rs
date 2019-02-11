d1c481b3579fa53b543d847dfe78bd0f3c49baa3
softprops <d.tangren@gmail.com>
2019-02-11 00:09:49 -0500

    helper methods for crate

--- xray/src/lambda.rs
+++ xray/src/lambda.rs
@@ -1,12 +1,23 @@
+use crate::Header;
 use std::{
     env::var,
     fs::{create_dir_all, File},
 };
 
 pub(crate) fn init() -> std::io::Result<()> {
-    if var("LAMBDA_TASK_ROOT").is_ok() {
+    if taskRoot().is_some() {
         create_dir_all("/tmp/.aws-xray")?;
         File::create("/tmp/.aws-xray/initialized")?;
     }
     Ok(())
+}
+
+pub(crate) fn taskRoot() -> Option<String> {
+    var("LAMBDA_TASK_ROOT").ok()
+}
+
+pub(crate) fn header() -> Option<Header> {
+    var("_X_AMZN_TRACE_ID")
+        .ok()
+        .and_then(|value| value.parse::<Header>().ok())
 }
